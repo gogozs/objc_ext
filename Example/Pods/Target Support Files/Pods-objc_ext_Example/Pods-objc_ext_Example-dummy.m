4acffea23e5c470b5b2a38460317c1e8eb4fb55d
Song Zhou <zhousong1993@gmail.com>
2018-04-19 17:09:44 +0800

    add example

---
+++ Example/Pods/Target Support Files/Pods-objc_ext_Example/Pods-objc_ext_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_objc_ext_Example : NSObject
+@end
+@implementation PodsDummy_Pods_objc_ext_Example
+@end
---
+++ Example/Pods/Target Support Files/objc_ext/objc_ext-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_objc_ext : NSObject
+@end
+@implementation PodsDummy_objc_ext
+@end
